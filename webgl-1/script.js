bf33b9246c1194f058cf5eb7290104bd8d3c24c9
Ra-V2 <rav.bernacki@gmail.com>
2024-05-17 21:39:58 +0200

    Added square, hexagon and button to randomize colors

--- webgl-1/script.js
+++ webgl-1/script.js
@@ -1,3 +1,5 @@
+//GLSL code
+
 const vertexShaderTxt = `
 precision mediump float;
 
@@ -25,12 +27,22 @@ void main() {
 }
 `
 
+// Main code, should be refactored to be more modular
+
 const mat4 = glMatrix.mat4;
 
-const Triangle = function () {
-    const canvas = document.getElementById('main-canvas');
+const LoadFigures = function () {
+    Triangle();
+    Square();
+    Hexagon();
+}
+
+
+
+const Triangle = function (colors = null) {
+    const canvas = document.getElementById('triangle-canvas');
     const gl = canvas.getContext('webgl');
-    let canvasColor = [0.2, 0.7, 0.5];
+    let canvasColor = [0.2, 0.2, 0.2];
 
     checkGl(gl);
 
@@ -65,11 +77,14 @@ const Triangle = function () {
 		0.5, -0.5, 
     ]
 
-    let colors = [
-        1.0, 0.0, 0.0,
-        0.0, 1.0, 0.0,
-        0.0, 0.0, 1.0
-    ]
+    if(colors != null)
+        colors = RandomizeColors();
+    else
+        colors = [
+            1.0, 0.0, 0.0,
+            0.0, 1.0, 0.0,
+            0.0, 0.0, 1.0
+        ]
 
     const triangleVertBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertBuffer);
@@ -139,6 +154,267 @@ const Triangle = function () {
 
     requestAnimationFrame(loop);
 }
+
+const Square = function (colors = null) {
+    const canvas = document.getElementById('square-canvas');
+    const gl = canvas.getContext('webgl');
+    let canvasColor = [0.2, 0.2, 0.2];
+
+    checkGl(gl);
+
+    gl.clearColor(...canvasColor, 1.0);  // R,G,B, A 
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    const vertexShader = gl.createShader(gl.VERTEX_SHADER);
+    const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+
+    gl.shaderSource(vertexShader, vertexShaderTxt);
+    gl.shaderSource(fragmentShader, fragmentShaderTxt);
+
+    gl.compileShader(vertexShader);
+    gl.compileShader(fragmentShader);
+
+    const program = gl.createProgram();
+
+    gl.attachShader(program, vertexShader);
+    gl.attachShader(program, fragmentShader);
+
+    gl.linkProgram(program);
+
+    gl.detachShader(program, vertexShader);
+    gl.detachShader(program, fragmentShader);
+
+    gl.validateProgram(program);
+
+    var squareVerts = [
+        -0.5,  0.5,
+        -0.5, -0.5,
+         0.5,  0.5,
+         0.5, -0.5,
+    ];
+
+    if(colors != null)
+        colors = RandomizeColors();
+    else
+        colors = [
+            1.0, 0.0, 0.0,
+            0.0, 1.0, 0.0,
+            0.0, 0.0, 1.0,
+            1.0, 1.0, 0.0
+        ]
+
+
+    const squareVertBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, squareVertBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(squareVerts), gl.STATIC_DRAW);
+
+    const posAttrLoc = gl.getAttribLocation(program, 'vertPosition');
+    gl.vertexAttribPointer(
+        posAttrLoc,
+        2,
+        gl.FLOAT,
+        gl.FALSE,
+        0,
+        0
+    );
+
+    gl.enableVertexAttribArray(posAttrLoc);
+
+    const squareColorBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, squareColorBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
+
+    const colorAttrLoc = gl.getAttribLocation(program, 'vertColor');
+    gl.vertexAttribPointer(
+        colorAttrLoc,
+        3,
+        gl.FLOAT,
+        gl.FALSE,
+        3 * Float32Array.BYTES_PER_ELEMENT,
+        0
+    );
+
+    gl.enableVertexAttribArray(colorAttrLoc);
+
+    // render time
+
+    gl.useProgram(program);
+
+    const worldMatLoc = gl.getUniformLocation(program, 'mWorld');
+    const viewMatLoc = gl.getUniformLocation(program, 'mView');
+    const projMatLoc = gl.getUniformLocation(program, 'mProjection');
+
+    const worldMatrix  = mat4.create();
+    const viewMatrix  = mat4.create();
+    mat4.lookAt(viewMatrix, [0,0,-1.5], [0,0,0], [0,1,0]); // vectors are: position of the camera, which way they are looking, which way is up
+    const projectionMatrix  = mat4.create();
+    mat4.perspective(projectionMatrix, glMatrix.glMatrix.toRadian(90), 
+                canvas.width / canvas.height, 0.1, 1000.0);
+
+    gl.uniformMatrix4fv(worldMatLoc, gl.FALSE, worldMatrix);
+    gl.uniformMatrix4fv(viewMatLoc, gl.FALSE, viewMatrix);
+    gl.uniformMatrix4fv(projMatLoc, gl.FALSE, projectionMatrix);
+
+    const identityMat = mat4.create();
+
+    const loop  = function () {
+        angle = performance.now() / 1000 / 60 * 23 * Math.PI;
+        mat4.rotate(worldMatrix, identityMat, angle, [1,1,1]);
+        gl.uniformMatrix4fv(worldMatLoc, gl.FALSE, worldMatrix);
+        
+        gl.clearColor(...canvasColor, 1.0);  // R,G,B, A 
+        gl.clear(gl.COLOR_BUFFER_BIT);
+
+        gl.drawArrays(gl.TRIANGLES, 0, 3);
+        gl.drawArrays(gl.TRIANGLES, 1, 3);
+        
+
+        requestAnimationFrame(loop);
+    }
+
+    requestAnimationFrame(loop);
+}
+
+const Hexagon = function (colors = null) {
+    const canvas = document.getElementById('hexagon-canvas');
+    const gl = canvas.getContext('webgl');
+    let canvasColor = [0.2, 0.2, 0.2];
+
+    checkGl(gl);
+
+    gl.clearColor(...canvasColor, 1.0);  // R,G,B, A 
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    const vertexShader = gl.createShader(gl.VERTEX_SHADER);
+    const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+
+    gl.shaderSource(vertexShader, vertexShaderTxt);
+    gl.shaderSource(fragmentShader, fragmentShaderTxt);
+
+    gl.compileShader(vertexShader);
+    gl.compileShader(fragmentShader);
+
+    const program = gl.createProgram();
+
+    gl.attachShader(program, vertexShader);
+    gl.attachShader(program, fragmentShader);
+
+    gl.linkProgram(program);
+
+    gl.detachShader(program, vertexShader);
+    gl.detachShader(program, fragmentShader);
+
+    gl.validateProgram(program);
+
+    var hexagonVerts = [
+        0.0, 0.5,
+        -0.5, 0.25,
+        -0.5, -0.25,
+        0.0, -0.5,
+        0.5, -0.25,
+        0.5, 0.25
+    ];
+
+    if(colors != null)
+        colors = RandomizeColors();
+    else
+        colors = [
+            1.0, 0.0, 0.0,
+            0.0, 0.0, 0.0,
+            0.0, 0.0, 0.0,
+            1.0, 0.5, 0.0,
+            1.0, 0.0, 0.0,
+            1.0, 0.0, 0.0
+        ];
+
+    const hexagonVertBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, hexagonVertBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(hexagonVerts), gl.STATIC_DRAW);
+
+    const posAttrLoc = gl.getAttribLocation(program, 'vertPosition');
+    gl.vertexAttribPointer(
+        posAttrLoc,
+        2,
+        gl.FLOAT,
+        gl.FALSE,
+        0,
+        0
+    );
+
+    gl.enableVertexAttribArray(posAttrLoc);
+
+    const hexagonColorBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, hexagonColorBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
+
+    const colorAttrLoc = gl.getAttribLocation(program, 'vertColor');
+    gl.vertexAttribPointer(
+        colorAttrLoc,
+        3,
+        gl.FLOAT,
+        gl.FALSE,
+        3 * Float32Array.BYTES_PER_ELEMENT,
+        0
+    );
+
+    gl.enableVertexAttribArray(colorAttrLoc);
+
+    // render time
+
+    gl.useProgram(program);
+
+    const worldMatLoc = gl.getUniformLocation(program, 'mWorld');
+    const viewMatLoc = gl.getUniformLocation(program, 'mView');
+    const projMatLoc = gl.getUniformLocation(program, 'mProjection');
+
+    const worldMatrix  = mat4.create();
+    const viewMatrix  = mat4.create();
+    mat4.lookAt(viewMatrix, [0,0,-1.5], [0,0,0], [0,1,0]); // vectors are: position of the camera, which way they are looking, which way is up
+    const projectionMatrix  = mat4.create();
+    mat4.perspective(projectionMatrix, glMatrix.glMatrix.toRadian(90), 
+                canvas.width / canvas.height, 0.1, 1000.0);
+
+    gl.uniformMatrix4fv(worldMatLoc, gl.FALSE, worldMatrix);
+    gl.uniformMatrix4fv(viewMatLoc, gl.FALSE, viewMatrix);
+    gl.uniformMatrix4fv(projMatLoc, gl.FALSE, projectionMatrix);
+
+    const identityMat = mat4.create();
+
+    const loop  = function () {
+        angle = performance.now() / 1000 / 60 * 23 * Math.PI;
+        mat4.rotate(worldMatrix, identityMat, angle, [1,1,0]);
+        gl.uniformMatrix4fv(worldMatLoc, gl.FALSE, worldMatrix);
+        
+        gl.clearColor(...canvasColor, 1.0);  // R,G,B, A 
+        gl.clear(gl.COLOR_BUFFER_BIT);
+
+        gl.drawArrays(gl.TRIANGLE_FAN, 0, 6);
+        
+
+        requestAnimationFrame(loop);
+    }
+
+    requestAnimationFrame(loop);
+}
+
+
+const RandomizeColors = function() {
+    let colors = [];
+    for (let i = 0; i < 18; i++) {
+        colors.push(Math.random());
+    }
+
+    return colors;
+
+}
+
+const ChangeColors = function() {
+    Triangle(RandomizeColors());
+    Square(RandomizeColors());
+    Hexagon(RandomizeColors());
+}
+
+// Helper functions
 
 function checkGl(gl) {
     if (!gl) {console.log('WebGL not supported, use another browser');}
